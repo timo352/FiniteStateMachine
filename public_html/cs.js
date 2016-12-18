e621456918486b1a14279eede855a44c03006690
alchemyfordummies <frobergtd@gmail.com>
2016-12-17 21:02:18 -0500

    Made more progress on text, added labels, added some todo items.

--- public_html/cs.js
+++ public_html/cs.js
@@ -1,3 +1,7 @@
+//TODO: fix text output, figure out how to get user input
+//TODO: fix the arrows, it prints 1 on arrow mode, none when states have been made
+//TODO: arrow does weird stuff when you try to put in states nearby
+
 /*
 Basic structure of our state machine:
 
@@ -6,20 +10,23 @@ Basic structure of our state machine:
 - each state has a coordinate and a radius for size
 - each arrow has a set of starting coordinates and ending coordinates
     (a coordinate is just an x and y point)
--
+- each arrow has an arrowhead that has to be drawn on at the end of it
+    (this needs some fixing)
+- we also have a label we can associate with either states or arrows
  */
 function Graph() {
     this.states = [];
     this.arrows = [];
+    this.labels = [];
 }
 
 function State(coord, rad, text) {
     this.location = new Coordinate(coord.x, coord.y);
     this.radius = rad;
-    this.name = (text == undefined ? new Label("") : new Label(text));
+    this.name = (text == undefined ? new Label("", this) : new Label(text, this));
 }
 
-function Arrow(coord1, coord2) {
+function Arrow(coord1, coord2, text ) {
     this.startState = undefined;
     this.endState = undefined;
 
@@ -28,6 +35,8 @@ function Arrow(coord1, coord2) {
     this.startCoord = new Coordinate(coord1.x, coord1.y);
     if (coord2 != undefined) this.endCoord = new Coordinate(coord2.x, coord2.y);
     else this.endCoord = new Coordinate();
+
+    this.label = (text == undefined ? new Label("", this) : new Label(text, this));
 
     this.complete = false;
 }
@@ -57,20 +66,27 @@ function Coordinate(ex, why) {
     this.y = why;
 }
 
-function Label(text) {
+function Label(text, obj, type, location) {
     this.fontSize = TEXT_SIZE;
     this.text = text;
+    this.associate = obj;
+    this.isState = (type == 'State');
+    this.location = location;
 }
 
 //CIRCLE_SIZE is a placeholder, the size should be customizable
-//TBL is how far one point of the triangle base is from coord2
+//TEXT_SIZE is basically the same
+//the two buffer variables are just to give some room between
+//states or between the text and its label/state
 const CIRCLE_SIZE = 120;
 const TEXT_SIZE = 14;
+const FONT_BUFFER = 5;
+const STATE_BUFFER = 15;
 const TRIANGLE_BASE_LENGTH = 3; //(5 * (Math.sin(Math.PI / 6))) / (Math.sin(Math.PI / 3))
                                 //but rounded up because pixels
 const TRIANGLE_BASE_X_CHANGE = 2;   //(3 * (Math.sin(Math.PI / 6))) / (Math.sin(Math.PI / 3))
                                     //but rounded up because pixels
-const TRIANGLE_CENTRAL_LENGTH = 5;
+const TRIANGLE_CENTRAL_LENGTH = 5; //5 units longer than the line segment
 
 //initialize our starting variables
 var graph = new Graph();
@@ -83,7 +99,6 @@ var moveMode = false;
 //make a canvas, do anti-aliasing
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    var mouseIsPressed = false;
     smooth(8);
 }
 
@@ -94,7 +109,7 @@ function draw() {
     strokeWeight(3);
 
     //if we've clicked on a button, we can start printing our states
-    //and arrows
+    //and arrows and labels
     if (start) {
         for (var i = 0; i < graph.states.length; ++i) {
             var state = graph.states[i];
@@ -109,14 +124,11 @@ function draw() {
                 triangle(ah.a.x, ah.a.y, ah.c.x, ah.c.y, ah.b.x, ah.b.y);
             }
         }
-    }
-}
 
-if (mouse) {
-    if (mouseButton == LEFT) {
-        leftClick();
-    } else if (mouseButton == RIGHT) {
-        rightClick();
+        for (var y = 0; y < graph.labels.length; y++) {
+            var label = graph.labels[y];
+            text(label.text, label.location.x, label.location.y);
+        }
     }
 }
 
@@ -124,52 +136,60 @@ if (mouse) {
 //get the mouse coordinates
 //if we've already clicked a button, we check the conditions of our buttons
 //make new state/arrow or finish an arrow as applicable
-function leftClick() {
+function mousePressed() {
     var x = mouseX;
     var y = mouseY;
     var temp;
 
+    //start means we've hit a button
     if (start) {
+        //draw circles
         if (!endArrow && circles) {
+            //did we click on an existing state?
             var state = whichState(new Coordinate(mouseX, mouseY));
+            //no, create a new state
             if (state == -1) {
                 temp = new State(new Coordinate(x, y), CIRCLE_SIZE);
                 graph.states.push(temp);
             } else {
-                name(graph.states[state]);
+                //yes, rename the state
+                name(graph.states[state], 'State');
             }
-
+        //first click for an arrow
         } else if (!endArrow && !circles) {
             temp = new Arrow(new Coordinate(x, y));
             graph.arrows.push(temp);
             endArrow = true;
+        //second click of an arrow
         } else if (endArrow && !circles) {
+            //set the ending coordinates for the arrow
             temp = graph.arrows[graph.arrows.length - 1];
             temp.endCoord.x = x;
             temp.endCoord.y = y;
+            //our arrow is done and printable
             temp.complete = true;
             temp.head = new ArrowHead(temp.startCoord, temp.endCoord);
+            //ready to start a new arrow
             endArrow = false;
         }
     } else if (moveMode) {
-
+        //TODO
     }
 
 }
 
-function rightClick() {
-
-}
-
+//returns which state was clicked on
+//returns a -1 if there was no state
 function whichState(coord) {
     ex = coord.x;
     why = coord.y;
     for (var i = 0; i < graph.states.length; i++) {
         var state = graph.states[i];
         var r = state.radius;
-        //distance between two points
+        //distance between two points, giving a little buffer room
+        //between states close together
         if (r > (Math.sqrt(Math.pow(ex - state.location.x, 2) +
-                Math.pow(why - state.location.y, 2)))) {
+                Math.pow(why - state.location.y, 2))) - STATE_BUFFER) {
             return i;
         }
     }
@@ -177,13 +197,28 @@ function whichState(coord) {
     return -1;
 }
 
-function slope(coord1, coord2) {
-    return (coord1.y - coord2.y) / (coord2.x  - coord1.x);
-}
+//Sets up our label
+function name(obj, type) {
+    //puts the text above the state but with a little bit of room
+    var temp;
+    if (type == 'State') {
+        //label with default text, a lebel for a state, and a defined location
+        //based on its associate object
+        temp = new Label("WORD", obj, 'State', new Coordinate(obj.location.x + obj.radius +
+                        obj.name.fontSize / 2 + FONT_BUFFER, obj.location.y));
+        obj.name = "WORD";
+    } else if (type == 'Arrow') {
+        //same condition but for an arrow
+        obj = graph.arrows[index];
+        obj.name = "WORD";
+    } else {
+        //I don't know how this would happen
+        alert("Somehow we passed this a bad name.");
+        return;
+    }
 
-function name(coord) {
-    var location = this.location;
-
+    //push the new label
+    graph.labels.push(temp);
 }
 
 //on click of the state button
@@ -200,4 +235,6 @@ function drawArrows() {
     start = true;
 }
 
+//take this out if you want to, I was just using this
+//for some console output
 var debug = true;
